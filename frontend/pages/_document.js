54ba03768f82876bfdf0fd4223f74109bda08879
dompaulich <domagoj.paulic@robinpowered.com>
2023-12-05 14:17:32 +0100

    style header component

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,15 +1,13 @@
-import React from 'react';
 import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        {/* <Head></Head> */}
+        <Head />
         <body>
-          <Main>
-            <NextScript />
-          </Main>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
