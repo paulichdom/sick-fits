4ee36ddb5dea3894ef8349b9ee222b0bc4150d19
Domagoj <paulic.domagoj@gmail.com>
2024-01-05 08:30:44 +0100

    update items

---
+++ frontend/pages/update.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UpdateProduct from '../components/UpdateProduct';
+
+
+export default function UpdatePage({ query }) {
+  console.log({ query });
+  return (
+    <div>
+      <UpdateProduct id={query.id} />
+    </div>
+  );
+}
