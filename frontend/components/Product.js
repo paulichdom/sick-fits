3812c60f70c1f0bb2b16016854a5aabc1cd1a035
Domagoj <paulic.domagoj@gmail.com>
2024-01-06 19:31:49 +0100

    impl delete product mutation (30)

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -4,6 +4,7 @@ import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 
 export default function Product({ product }) {
   return (
@@ -17,7 +18,7 @@ export default function Product({ product }) {
       <div className="buttonList">
         <Link
           href={{
-            pathname: 'update',
+            pathname: '/update',
             query: {
               id: product.id,
             },
@@ -25,6 +26,7 @@ export default function Product({ product }) {
         >
           Edit
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
   );
--- frontend/pages/update.js
+++ frontend/pages/update.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import UpdateProduct from '../components/UpdateProduct';
 
-
 export default function UpdatePage({ query }) {
-  console.log({ query });
   return (
     <div>
       <UpdateProduct id={query.id} />
