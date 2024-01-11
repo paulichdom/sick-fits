a7b346d660c0feca37c30372d4e7265096b24004
Domagoj <paulic.domagoj@gmail.com>
2024-01-11 08:48:59 +0100

    query current user

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
+  console.log({ user });
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Order</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Order</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
