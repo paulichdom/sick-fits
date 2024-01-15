552ffe3cb88798878f309c9961a22f484dbad761
Domagoj <paulic.domagoj@gmail.com>
2024-01-15 08:56:33 +0100

    request a password reset (40)

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import RequestReset from '../components/RequestReset';
 
 const GridStyles = styled.div`
   display: grid;
@@ -14,6 +15,7 @@ export default function SignInPage() {
     <GridStyles>
       <SignIn />
       <SignUp />
+      <RequestReset />
     </GridStyles>
   );
 }
