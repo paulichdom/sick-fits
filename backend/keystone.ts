8dcb15db5fbf50b67168b56d2b91982d24daa849
Domagoj <paulic.domagoj@gmail.com>
2024-01-26 10:10:07 +0100

    create data type and relationships (43)

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -10,6 +10,7 @@ import { User } from './schemas/User';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { CartItem } from './schemas/CartItem';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -58,6 +59,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem
     }),
     ui: {
       // Show the UI only for poeple who pass this test
